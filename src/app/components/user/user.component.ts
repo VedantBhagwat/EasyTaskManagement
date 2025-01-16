6b5b761a9f8b3f3fd2eff314fb4fc47940cb7002
VedantBhagwat <vedant143bhagwat@gmail.com>
2025-01-16 22:05:46 +0000

    use output and event emiiter

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,13 @@
-import { Component, computed, Input, input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  output,
+  Output,
+  signal,
+} from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -26,14 +35,20 @@ export class UserComponent {
   } */
 
   // Implementation using signals
+  @Input() id!: string;
   avatar = input.required<string>();
   name = input.required<string>();
+
+  // @Output() select = new EventEmitter<string>();
+  // Note: output is of type OutputEmitterRef and not a output signal
+  select = output<string>();
 
   selectedUser = signal(DUMMY_USERS[randomIndex]);
   imagePath = computed(() => 'assets/users/' + this.avatar());
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }

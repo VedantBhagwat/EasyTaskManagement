cfa56f26a78c8d7d73275f789d6970a889f62d74
VedantBhagwat <vedant143bhagwat@gmail.com>
2025-01-20 23:08:27 +0000

    transform due-date using DatePipe

--- src/app/components/tasks/task/task.component.ts
+++ src/app/components/tasks/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, output } from '@angular/core';
 import { type Task } from '../../../interfaces/task.model';
 import { CardComponent } from '../../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })

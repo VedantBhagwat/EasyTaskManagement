9246f16c3d2e494c6d2849755f3833dce7a6527a
VedantBhagwat <vedant143bhagwat@gmail.com>
2025-01-20 23:53:05 +0000

    use localstorage to store list of tasks

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -31,7 +31,12 @@ export class TasksService {
     },
   ];
 
-  constructor() {}
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
@@ -46,9 +51,15 @@ export class TasksService {
       summary: data.summary,
       dueDate: data.date,
     });
+    this.saveTasks();
   }
 
   completeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }

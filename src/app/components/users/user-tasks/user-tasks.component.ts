c4205328bfb4d50a9594ef5365be560ed907dcc9
VedantBhagwat <vedant143bhagwat@gmail.com>
2025-02-23 22:12:45 +0000

    display username using dynamic route parameters using input function

--- src/app/components/users/user-tasks/user-tasks.component.ts
+++ src/app/components/users/user-tasks/user-tasks.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   selector: 'app-user-tasks',
   imports: [],
   templateUrl: './user-tasks.component.html',
-  styleUrl: './user-tasks.component.scss'
+  styleUrl: './user-tasks.component.scss',
 })
 export class UserTasksComponent {
-
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+  userName = computed(
+    () => this.usersService.users.find((user) => user.id == this.userId())?.name
+  );
 }

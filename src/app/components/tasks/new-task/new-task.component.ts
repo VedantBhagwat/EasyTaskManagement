cf6e247181094c1a61855307ee2d198528130e1b
VedantBhagwat <vedant143bhagwat@gmail.com>
2025-02-24 12:17:20 +0000

    add routing to cancel button in newTaskComponent

--- src/app/components/tasks/new-task/new-task.component.ts
+++ src/app/components/tasks/new-task/new-task.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, input, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TasksService } from '../../../services/tasks.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-task',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss',
 })
@@ -17,6 +18,7 @@ export class NewTaskComponent {
   date = signal('');
 
   private tasksService = inject(TasksService);
+  private router = inject(Router);
 
   onSubmitNewTask() {
     this.tasksService.addTask(
@@ -27,5 +29,11 @@ export class NewTaskComponent {
       },
       this.userId()
     );
+
+    // navigate to users tasks page. {replaceUrl: true} makes sure that user don't go back
+    // to new task form page
+    this.router.navigate(['/users', this.userId(), 'tasks'], {
+      replaceUrl: true,
+    });
   }
 }
